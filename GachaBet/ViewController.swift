d9d8c6794f23391b02a2e9b1fde6e03d0a783760
Supanat Wanroj <supanat.wanroj@scb.co.th>
2020-11-20 19:19:12 +0700

    Initial Commit

---
+++ GachaBet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GachaBet
+//
+//  Created by SUPANAT WANROJ on 20/11/2563 BE.
+//  Copyright © 2563 SUPANAT WANROJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
